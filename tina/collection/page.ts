2a65b1cb24212d605063fb9cf7dabdec4801a368
Ryan Arrindell <ryanarrindell@googlemail.com>
2025-07-21 17:32:28 +0100

    Add test case

--- tina/collection/page.ts
+++ tina/collection/page.ts
@@ -1,4 +1,4 @@
-import type { Collection } from 'tinacms';
+import type { Collection, Template } from 'tinacms';
 import { heroBlockSchema } from '@/components/blocks/hero';
 import { contentBlockSchema } from '@/components/blocks/content';
 import { testimonialBlockSchema } from '@/components/blocks/testimonial';
@@ -7,6 +7,70 @@ import { videoBlockSchema } from '@/components/blocks/video';
 import { calloutBlockSchema } from '@/components/blocks/callout';
 import { statsBlockSchema } from '@/components/blocks/stats';
 import { ctaBlockSchema } from '@/components/blocks/call-to-action';
+
+export const layoutBlockSchema: Template = {
+  name: 'columLayout',
+  label: 'Column Layout',
+  ui: {
+    previewSrc: '/blocks/hero.png',
+  },
+  fields: [
+    {
+      type: 'object',
+      label: 'Components',
+      name: 'components',
+      list: true,
+      ui: {
+        visualSelector: true,
+      },
+      templates: [
+        {
+          name: 'featureCard',
+          label: 'Feature Card',
+          ui: {
+            previewSrc: '/blocks/hero.png',
+          },
+          fields: [
+            {
+              name: 'title',
+              label: 'Title',
+              type: 'string',
+            },
+            {
+              name: 'subtitle',
+              label: 'Subtitle',
+              type: 'string',
+            }
+          ],
+        },
+        {
+          name: 'productCard',
+          label: 'Product Card',
+          ui: {
+            previewSrc: '/blocks/hero.png',
+          },
+          fields: [
+            {
+              name: 'title',
+              label: 'Title',
+              type: 'string',
+            },
+            {
+              name: 'subtitle',
+              label: 'Subtitle',
+              type: 'string',
+            }
+          ],
+        }
+      ],
+    },
+    {
+      type: 'string',
+      label: 'Background',
+      name: 'background',
+    },
+  ],
+};
 
 const Page: Collection = {
   label: 'Pages',
@@ -40,6 +104,7 @@ const Page: Collection = {
         contentBlockSchema,
         testimonialBlockSchema,
         videoBlockSchema,
+        layoutBlockSchema,
       ],
     },
   ],
